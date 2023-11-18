6baa429874e14c8645b7c5a77d29cd027dbc3c03
WeazNet <vladalexandru.tanasov@gmail.com>
2023-11-18 11:39:55 +0100

    update: design MovieLibrary

--- src/design/atoms/MovieCard.tsx
+++ src/design/atoms/MovieCard.tsx
@@ -8,7 +8,7 @@ export const MovieCard = ({
   imageSrc: string;
 }) => {
   return (
-    <div title={title} key={id} className="bg-gray-800 relative rounded overflow-hidden shadow-lg hover:scale-105 w-min-content hover:after:block hover:after:absolute hover:after:left-0 hover:after:top-0 hover:after:z-50 hover:after:backdrop-blur-sm hover:after:min-w-full hover:after:min-h-full hover:after:text-white hover:after:content-[attr(title)] hover:after:transition hover:after:font-bold hover:after:text-center hover:after:pt-[60%] sm:text-xl text-2xl ">
+    <div title={title} key={id} className="bg-gray-800 relative rounded overflow-hidden shadow-lg hover:scale-105 w-min-content hover:after:block hover:after:absolute hover:after:left-0 hover:after:top-0 hover:after:z-50 hover:after:backdrop-blur-sm hover:after:min-w-full hover:after:min-h-full hover:after:text-white hover:after:content-[attr(title)] hover:after:transition hover:after:font-bold hover:after:text-center hover:after:pt-[60%] sm:text-xl text-2xl hover:after:px-4">
       <img className="w-full" src={imageSrc} alt={"image of " + title} />
     </div>
   );
--- src/features/MovieLibrary/components/MovieList.tsx
+++ src/features/MovieLibrary/components/MovieList.tsx
@@ -6,8 +6,14 @@ import { MovieCardSkeleton } from "../../../design/atoms/MovieCardSkeleton";
 import { BASE_PATH_IMAGE } from "../../../services/utils";
 
 function MovieList() {
-  const { isLoading, data: movies } = useQuery("[movies]", getNowPlayingMovies);
-
+  const {
+    isLoading,
+    error,
+    data: movies,
+  } = useQuery(["movies"], getNowPlayingMovies);
+  if (error) {
+    throw Error("Server is unreachable");
+  }
   return (
     <div className="px-5 py-5">
       <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-8">
