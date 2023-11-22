37e905c82da10238041de579745cd1af3f020dcf
WeazNet <vladalexandru.tanasov@gmail.com>
2023-11-22 21:53:42 +0100

    fix: navButtons

--- src/features/MovieLibrary/components/NavBar.tsx
+++ src/features/MovieLibrary/components/NavBar.tsx
@@ -1,11 +1,18 @@
+import { useContext } from "react";
 import { NavButton } from "../../../design/atoms/NavButton";
+import { SearchBarContext } from "../../../contexts/SearchBarContext";
 
 export const NavBar = () => {
+  const { inputValue } = useContext(SearchBarContext);
   return (
     <nav className="flex gap-2 flex-wrap my-4">
-      <NavButton to="/upcoming" name="📅 Upcoming 🔸 🇫🇷" />
-      <NavButton to="/" name="🚨 Now playing 🔸 🇫🇷" />
+      {(inputValue.length == 0 && (
+        <>
+          <NavButton to="/upcoming" name="📅 Upcoming 🔸 🇫🇷" />
+          <NavButton to="/" name="🚨 Now playing 🔸 🇫🇷" />
+        </>
+      )) || <NavButton to="/" name="🔎 Search" />}
       <NavButton to="/favorite" name="💯 Favorite" />
     </nav>
-  )
+  );
 };
