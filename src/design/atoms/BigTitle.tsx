bdde16a1797a6b507ff40c786200e02719a63cba
WeazNet <vladalexandru.tanasov@gmail.com>
2023-11-22 22:10:40 +0100

    fix: navButtons (2)

--- src/design/atoms/BigTitle.tsx
+++ src/design/atoms/BigTitle.tsx
@@ -1,3 +1,3 @@
 export const BigTitle = ({ content }: { content: string }) => {
-  return <h1 className="text-2xl text-justify text-gray-900 dark:text-white sm:text-4xl">{content}</h1>;
+  return <h1 className="mr-2 text-2xl text-justify text-gray-900 dark:text-white sm:text-4xl">{content}</h1>;
 };
--- src/features/MovieLibrary/components/NavBar.tsx
+++ src/features/MovieLibrary/components/NavBar.tsx
@@ -4,14 +4,19 @@ import { SearchBarContext } from "../../../contexts/SearchBarContext";
 
 export const NavBar = () => {
   const { inputValue } = useContext(SearchBarContext);
+  const location = window.location.pathname;
   return (
     <nav className="flex gap-2 flex-wrap my-4">
-      {(inputValue.length == 0 && (
+      {inputValue.length == 0 && (
         <>
           <NavButton to="/upcoming" name="📅 Upcoming 🔸 🇫🇷" />
           <NavButton to="/" name="🚨 Now playing 🔸 🇫🇷" />
         </>
-      )) || <NavButton to="/" name="🔎 Search" />}
+      )}
+      {inputValue.length != 0 && location != "/favorite" && (
+        <NavButton to={location} name="🔎 Search" />
+      )}
+
       <NavButton to="/favorite" name="💯 Favorite" />
     </nav>
   );
