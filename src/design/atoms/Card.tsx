e65267de13d24889cc6afaac7f80a07d8ad1a9d9
WeazNet <vladalexandru.tanasov@gmail.com>
2023-11-23 14:06:46 +0100

    fix: image load

--- src/design/atoms/Card.tsx
+++ src/design/atoms/Card.tsx
@@ -7,12 +7,17 @@ export const Card = ({
   title: string;
   imageSrc: string;
 }) => {
+  const handleLoad = (e: any) => {
+    e.target.src = imageSrc;
+  };
   return (
-    <div
-      key={id}
-      className="relative rounded overflow-hidden shadow-lg"
-    >
-      <img className="w-full" src={imageSrc} alt={"image of " + title} />
+    <div key={id} className="relative rounded overflow-hidden shadow-lg">
+      <img
+        onLoad={(e) => handleLoad(e)}
+        className="w-full"
+        src={"https://placehold.co/500x750?text=?&font=roboto"}
+        alt={"image of " + title}
+      />
     </div>
   );
 };
